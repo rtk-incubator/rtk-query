7d56fb3ef39e90dcb8832b9f897814f7fe4257b3
Matt Sutkowski <msutkowski@gmail.com>
2020-11-05 20:37:55 -0800

    Show arrow function style in svelte example, change fetchBaseQuery config

--- examples/svelte-example/src/services/counter.ts
+++ examples/svelte-example/src/services/counter.ts
@@ -1,56 +1,34 @@
-import { createApi } from '@rtk-incubator/simple-query/dist';
+import { createApi, fetchBaseQuery } from '@rtk-incubator/simple-query/dist';
 
 interface CountResponse {
     count: number;
 }
 
-interface QueryArg {
-    queryString: string;
-    method?: string;
-    body?: string;
-    headers?: any;
-}
-
 export const counterApi = createApi({
     reducerPath: 'counterApi',
-    baseQuery({
-        queryString = '',
-        method = 'GET',
-        body,
-        headers = {
-            'Content-Type': 'application/json',
-        },
-    }: QueryArg) {
-        return fetch(`/${queryString}`, { method, body, headers }).then((res) => res.json());
-    },
+    baseQuery: fetchBaseQuery(),
     entityTypes: ['Counter'],
     endpoints: (build) => ({
         getCount: build.query<CountResponse, void>({
-            query() {
-                return {
-                    queryString: `count`,
-                };
-            },
+            query: () => ({
+                queryString: `count`,
+            }),
             provides: [{ type: 'Counter' }],
         }),
         incrementCount: build.mutation<CountResponse, number>({
-            query(amount) {
-                return {
-                    queryString: `increment`,
-                    method: 'PUT',
-                    body: JSON.stringify({ amount }),
-                };
-            },
+            query: (amount) => ({
+                queryString: `increment`,
+                method: 'PUT',
+                body: JSON.stringify({ amount }),
+            }),
             invalidates: [{ type: 'Counter' }],
         }),
         decrementCount: build.mutation<CountResponse, number>({
-            query(amount) {
-                return {
-                    queryString: `decrement`,
-                    method: 'PUT',
-                    body: JSON.stringify({ amount }),
-                };
-            },
+            query: (amount) => ({
+                queryString: `decrement`,
+                method: 'PUT',
+                body: JSON.stringify({ amount }),
+            }),
             invalidates: [{ type: 'Counter' }],
         }),
     }),

d4ef3f14c006f33477ae07d1ce3751be9ab402da
Matt Sutkowski <msutkowski@gmail.com>
2020-11-12 23:08:14 -0800

    Add utils

---
+++ examples/svelte/src/utils/pollingOptions.ts
@@ -0,0 +1,7 @@
+export const pollingOptions = [
+    { value: 0, label: '0 - off' },
+    { value: 1000, label: '1s' },
+    { value: 5000, label: '5s' },
+    { value: 10000, label: '10s' },
+    { value: 60000, label: '1m' },
+];

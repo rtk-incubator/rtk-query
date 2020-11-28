04f038636603e78ee114d8c49646d0bd19f6817e
Matt Sutkowski <msutkowski@gmail.com>
2020-11-28 07:16:47 -0800

    Add getState usage, a little cleanup

--- test/fetchBaseQuery.test.tsx
+++ test/fetchBaseQuery.test.tsx
@@ -1,3 +1,4 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { createApi, fetchBaseQuery } from '@rtk-incubator/rtk-query';
 import { setupApiStore } from './helpers';
 
@@ -7,9 +8,25 @@ const defaultHeaders: Record<string, string> = {
   delete2: '1',
 };
 
+let fetchSpy: jest.SpyInstance<any, any>;
+
+beforeAll(() => {
+  fetchSpy = jest.spyOn(global, 'fetch');
+});
+
+afterEach(() => {
+  fetchSpy.mockClear();
+});
+
 const baseQuery = fetchBaseQuery({
   baseUrl: 'http://example.com',
   prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
     // A user could customize their behavior here, so we'll just test that custom scenarios would work.
     const potentiallyConflictingKeys = Object.keys(defaultHeaders);
     potentiallyConflictingKeys.forEach((key) => {
@@ -37,38 +54,38 @@ const api = createApi({
   },
 });
 
-const storeRef = setupApiStore(api);
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    token: '',
+  },
+  reducers: {
+    setToken(state, action) {
+      state.token = action.payload;
+    },
+  },
+});
+
+const storeRef = setupApiStore(api, { auth: authSlice.reducer });
+type RootState = ReturnType<typeof storeRef.store.getState>;
 
 describe('fetchBaseQuery', () => {
   test('uses the default headers set in prepareHeaders', async () => {
-    const spy = jest.spyOn(global, 'fetch');
-
     await baseQuery('/success', {
       signal: undefined,
       dispatch: storeRef.store.dispatch,
       getState: storeRef.store.getState,
     });
 
-    const [, options] = spy.mock.calls[0];
-    if (!options?.headers) {
-      throw Error('Missing headers');
-    }
-    const headersObj: Record<string, string> = {};
-    new Headers(options.headers).forEach((value, key) => {
-      headersObj[key] = value;
-    });
+    const [, options] = fetchSpy.mock.calls[0];
 
-    expect(headersObj).toHaveProperty('content-type', 'application/json');
-    expect(headersObj).toHaveProperty('fake', 'header');
-    expect(headersObj).toHaveProperty('delete', 'true');
-    expect(headersObj).toHaveProperty('delete2', '1');
-
-    spy.mockClear();
+    expect(options!.headers.get('content-type')).toBe('application/json');
+    expect(options!.headers.get('fake')).toBe(defaultHeaders['fake']);
+    expect(options!.headers.get('delete')).toBe(defaultHeaders['delete']);
+    expect(options!.headers.get('delete2')).toBe(defaultHeaders['delete2']);
   });
 
   test('adds endpoint-level headers to the defaults', async () => {
-    const spy = jest.spyOn(global, 'fetch');
-
     await baseQuery(
       { url: '/success', headers: { authorization: 'Bearer banana' } },
       {
@@ -77,28 +94,16 @@ describe('fetchBaseQuery', () => {
         getState: storeRef.store.getState,
       }
     );
-    const [, options] = spy.mock.calls[0];
+    const [, options] = fetchSpy.mock.calls[0];
 
-    if (!options?.headers) {
-      throw Error('Missing headers');
-    }
-    const headersObj: Record<string, string> = {};
-    new Headers(options.headers).forEach((value, key) => {
-      headersObj[key] = value;
-    });
-
-    expect(headersObj).toHaveProperty('authorization', 'Bearer banana');
-    expect(headersObj).toHaveProperty('content-type', 'application/json');
-    expect(headersObj).toHaveProperty('fake', defaultHeaders['fake']);
-    expect(headersObj).toHaveProperty('delete', defaultHeaders['delete']);
-    expect(headersObj).toHaveProperty('delete2', defaultHeaders['delete2']);
-
-    spy.mockClear();
+    expect(options!.headers.get('authorization')).toBe('Bearer banana');
+    expect(options!.headers.get('content-type')).toBe('application/json');
+    expect(options!.headers.get('fake')).toBe(defaultHeaders['fake']);
+    expect(options!.headers.get('delete')).toBe(defaultHeaders['delete']);
+    expect(options!.headers.get('delete2')).toBe(defaultHeaders['delete2']);
   });
 
   test('it does not set application/json when content-type is set', async () => {
-    const spy = jest.spyOn(global, 'fetch');
-
     await baseQuery(
       { url: '/success', headers: { authorization: 'Bearer banana', 'content-type': 'custom-content-type' } },
       {
@@ -107,27 +112,21 @@ describe('fetchBaseQuery', () => {
         getState: storeRef.store.getState,
       }
     );
-    const [, options] = spy.mock.calls[0];
-    if (!options?.headers) {
-      throw Error('Missing headers');
-    }
+    const [, options] = fetchSpy.mock.calls[0];
+
     const headersObj: Record<string, string> = {};
-    new Headers(options.headers).forEach((value, key) => {
+    new Headers(options!.headers).forEach((value, key) => {
       headersObj[key] = value;
     });
 
-    expect(headersObj).toEqual({
-      authorization: 'Bearer banana',
-      'content-type': 'custom-content-type',
-      ...defaultHeaders,
-    });
-
-    spy.mockClear();
+    expect(options!.headers.get('authorization')).toBe('Bearer banana');
+    expect(options!.headers.get('content-type')).toBe('custom-content-type');
+    expect(options!.headers.get('fake')).toBe(defaultHeaders['fake']);
+    expect(options!.headers.get('delete')).toBe(defaultHeaders['delete']);
+    expect(options!.headers.get('delete2')).toBe(defaultHeaders['delete2']);
   });
 
   test('respects the headers from an endpoint over the base headers', async () => {
-    const spy = jest.spyOn(global, 'fetch');
-
     const fake = 'fake endpoint value';
 
     await baseQuery(
@@ -138,19 +137,45 @@ describe('fetchBaseQuery', () => {
         getState: storeRef.store.getState,
       }
     );
-    const [, options] = spy.mock.calls[0];
-    if (!options?.headers) {
-      throw Error('Missing headers');
-    }
+    const [, options] = fetchSpy.mock.calls[0];
+
     const headersObj: Record<string, string> = {};
-    new Headers(options.headers).forEach((value, key) => {
+    new Headers(options!.headers).forEach((value, key) => {
       headersObj[key] = value;
     });
 
-    expect(headersObj).toHaveProperty('fake', fake);
-    expect(headersObj).not.toHaveProperty('delete');
-    expect(headersObj).not.toHaveProperty('delete2');
+    expect(options!.headers.get('fake')).toBe(fake);
+    expect(options!.headers.get('delete')).toBeNull();
+    expect(options!.headers.get('delete2')).toBeNull();
+  });
 
-    spy.mockClear();
+  test('prepareHeaders is able to select from a state', async () => {
+    const doRequest = async () =>
+      await baseQuery(
+        { url: '/success' },
+        {
+          signal: undefined,
+          dispatch: storeRef.store.dispatch,
+          getState: storeRef.store.getState,
+        }
+      );
+
+    doRequest();
+
+    let [, options] = fetchSpy.mock.calls[0];
+
+    expect(options!.headers.get('authorization')).toBeNull();
+
+    // set a token and the follow up request should have the header injected by prepareHeaders
+    const token = 'fakeToken!';
+    storeRef.store.dispatch(authSlice.actions.setToken(token));
+    doRequest();
+
+    [, options] = fetchSpy.mock.calls[1];
+    if (!options?.headers) {
+      throw Error('Missing headers');
+    }
+
+    expect(options.headers.get('authorization')).toBe(`Bearer ${token}`);
   });
 });

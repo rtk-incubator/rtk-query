e6967f7e7f75a1d67b32c6de325089561d9e1e65
Matt Sutkowski <msutkowski@gmail.com>
2020-11-29 22:52:15 -0800

    Add a basic exponential backoff, additional tests and helpers, example

--- test/retryStaggered.test.ts
+++ test/retryStaggered.test.ts
@@ -1,5 +1,18 @@
-import { BaseQueryFn, createApi, retryStaggered } from '@rtk-incubator/rtk-query';
+import { BaseQueryFn, createApi, retryStaggered, withoutStaggering } from '@rtk-incubator/rtk-query';
 import { setupApiStore, waitMs } from './helpers';
+
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+const loopTimers = async (max: number = 12) => {
+  let count = 0;
+  while (count < max) {
+    await waitMs(1);
+    jest.runOnlyPendingTimers();
+    count++;
+  }
+};
 
 describe('configuration', () => {
   test('staggering without any config', async () => {
@@ -19,7 +32,7 @@ describe('configuration', () => {
     const storeRef = setupApiStore(api);
     storeRef.store.dispatch(api.endpoints.q1.initiate({}));
 
-    await waitMs(3);
+    await loopTimers(7);
 
     expect(baseBaseQuery).toHaveBeenCalledTimes(6);
   });
@@ -41,7 +54,7 @@ describe('configuration', () => {
     const storeRef = setupApiStore(api);
     storeRef.store.dispatch(api.endpoints.q1.initiate({}));
 
-    await waitMs(3);
+    await loopTimers(5);
 
     expect(baseBaseQuery).toHaveBeenCalledTimes(4);
   });
@@ -67,19 +80,151 @@ describe('configuration', () => {
     const storeRef = setupApiStore(api);
 
     storeRef.store.dispatch(api.endpoints.q1.initiate({}));
-    await waitMs(3);
+    await loopTimers(5);
+
     expect(baseBaseQuery).toHaveBeenCalledTimes(4);
 
     baseBaseQuery.mockClear();
 
     storeRef.store.dispatch(api.endpoints.q2.initiate({}));
-    await waitMs(3);
+
+    await loopTimers(10);
+
     expect(baseBaseQuery).toHaveBeenCalledTimes(9);
   });
 
-  test.todo('same with mutations');
-  test.todo('non-error-cases should **not** retry');
-  test.todo('throwing `withoutStaggering(error)` will skip staggering and expose the error directly');
+  test('staggered retries also work with mutations', async () => {
+    const baseBaseQuery = jest.fn<ReturnType<BaseQueryFn>, Parameters<BaseQueryFn>>();
+    baseBaseQuery.mockRejectedValue(new Error('rejected'));
 
-  test.todo('wrapping retryStaggered(retryStaggered(..., { maxRetries: 3 }), { maxRetries: 3 }) should retry 9 times');
+    const baseQuery = retryStaggered(baseBaseQuery, { maxRetries: 3 });
+    const api = createApi({
+      baseQuery,
+      endpoints: (build) => ({
+        m1: build.mutation({
+          query: () => ({ method: 'PUT' }),
+        }),
+      }),
+    });
+
+    const storeRef = setupApiStore(api);
+
+    storeRef.store.dispatch(api.endpoints.m1.initiate({}));
+
+    await loopTimers(5);
+
+    expect(baseBaseQuery).toHaveBeenCalledTimes(4);
+  });
+  test('non-error-cases should **not** retry', async () => {
+    const baseBaseQuery = jest.fn<ReturnType<BaseQueryFn>, Parameters<BaseQueryFn>>();
+    baseBaseQuery.mockResolvedValue({ data: { success: true } });
+
+    const baseQuery = retryStaggered(baseBaseQuery, { maxRetries: 3 });
+    const api = createApi({
+      baseQuery,
+      endpoints: (build) => ({
+        q1: build.query({
+          query: () => {},
+        }),
+      }),
+    });
+
+    const storeRef = setupApiStore(api);
+
+    storeRef.store.dispatch(api.endpoints.q1.initiate({}));
+
+    await loopTimers(2);
+
+    expect(baseBaseQuery).toHaveBeenCalledTimes(1);
+  });
+  test('throwing withoutStaggering(error) will skip staggering and expose the error directly', async () => {
+    const error = { message: 'banana' };
+
+    const baseBaseQuery = jest.fn<ReturnType<BaseQueryFn>, Parameters<BaseQueryFn>>();
+    baseBaseQuery.mockRejectedValue(error);
+
+    const baseQuery = withoutStaggering(baseBaseQuery);
+    const api = createApi({
+      baseQuery,
+      endpoints: (build) => ({
+        q1: build.query({
+          query: () => {},
+        }),
+      }),
+    });
+
+    const storeRef = setupApiStore(api);
+
+    const result = await storeRef.store.dispatch(api.endpoints.q1.initiate({}));
+
+    await loopTimers(2);
+
+    expect(baseBaseQuery).toHaveBeenCalledTimes(1);
+    expect(result.error).toEqual(error);
+    expect(result).toEqual({
+      endpoint: 'q1',
+      error,
+      internalQueryArgs: undefined,
+      isError: true,
+      isLoading: false,
+      isSuccess: false,
+      isUninitialized: false,
+      originalArgs: expect.any(Object),
+      requestId: expect.any(String),
+      startedTimeStamp: expect.any(Number),
+      status: 'rejected',
+    });
+  });
+
+  test('wrapping retryStaggered(retryStaggered(..., { maxRetries: 3 }), { maxRetries: 3 }) should retry 16 times', async () => {
+    /**
+     * Note:
+     * This will retry 16 total times because we try the initial + 3 retries (sum: 4), then retry that process 3 times (starting at 0 for a total of 4)... 4x4=16 (allegedly)
+     */
+    const baseBaseQuery = jest.fn<ReturnType<BaseQueryFn>, Parameters<BaseQueryFn>>();
+    baseBaseQuery.mockRejectedValue(new Error('rejected'));
+
+    const baseQuery = retryStaggered(retryStaggered(baseBaseQuery, { maxRetries: 3 }), { maxRetries: 3 });
+    const api = createApi({
+      baseQuery,
+      endpoints: (build) => ({
+        q1: build.query({
+          query: () => {},
+        }),
+      }),
+    });
+
+    const storeRef = setupApiStore(api);
+
+    storeRef.store.dispatch(api.endpoints.q1.initiate({}));
+
+    await loopTimers(18);
+
+    expect(baseBaseQuery).toHaveBeenCalledTimes(16);
+  });
+
+  test('stops retrying after a success', async () => {
+    const baseBaseQuery = jest.fn<ReturnType<BaseQueryFn>, Parameters<BaseQueryFn>>();
+    baseBaseQuery
+      .mockRejectedValueOnce(new Error('rejected'))
+      .mockRejectedValueOnce(new Error('rejected'))
+      .mockResolvedValue({ data: { success: true } });
+
+    const baseQuery = retryStaggered(baseBaseQuery, { maxRetries: 10 });
+    const api = createApi({
+      baseQuery,
+      endpoints: (build) => ({
+        q1: build.query({
+          query: () => {},
+        }),
+      }),
+    });
+
+    const storeRef = setupApiStore(api);
+    storeRef.store.dispatch(api.endpoints.q1.initiate({}));
+
+    await loopTimers(4);
+
+    expect(baseBaseQuery).toHaveBeenCalledTimes(3);
+  });
 });

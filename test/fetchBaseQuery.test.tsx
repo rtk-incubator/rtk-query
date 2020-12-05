f72fa8e0c9f75ce1183db1518e3adaec49078622
Aaron Pettengill <aaron.pettengill@echoman.com>
2020-12-05 16:58:48 -0500

    Return null for empty json response

--- test/fetchBaseQuery.test.tsx
+++ test/fetchBaseQuery.test.tsx
@@ -89,6 +89,22 @@ describe('fetchBaseQuery', () => {
       expect(res.data).toEqual({ value: 'success' });
     });
 
+    it('should return null for a simple GET request when the response is null', async () => {
+      const req = baseQuery(
+        '/empty',
+        {
+          signal: undefined,
+          dispatch: storeRef.store.dispatch,
+          getState: storeRef.store.getState,
+        },
+        {}
+      );
+      expect(req).toBeInstanceOf(Promise);
+      const res = await req;
+      expect(res).toBeInstanceOf(Object);
+      expect(res.data).toBeNull();
+    });
+
     it('should return an error and status for error responses', async () => {
       const req = baseQuery(
         '/error',

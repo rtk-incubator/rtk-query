db25323cd78648dbb9258c35c7ce58235c7b7974
Lenz Weber <mail@lenzw.de>
2020-12-13 14:41:13 +0100

    cleanUndefinedHeaders method simplify type

--- src/fetchBaseQuery.ts
+++ src/fetchBaseQuery.ts
@@ -1,78 +1,14 @@
 import { joinUrls } from './utils';
 import { isPlainObject } from '@reduxjs/toolkit';
 import { BaseQueryFn } from './apiTypes';
+import { Override } from './tsHelpers';
 
 export type ResponseHandler = 'json' | 'text' | ((response: Response) => Promise<any>);
 
-// We need to control most of these types to allow for more flexible headers
-interface Headers {
-  append(name: string, value: string): void;
-  delete(name: string): void;
-  get(name: string): string | null;
-  has(name: string): boolean;
-  set(name: string, value: string): void;
-  forEach(callbackfn: (value: string, key: string, parent: Headers) => void, thisArg?: any): void;
-}
-
-declare var Headers: {
-  prototype: Headers;
-  new (init?: CustomHeadersInit): Headers;
-};
-export type CustomHeadersInit = Headers | string[][] | Record<string, string | undefined>;
-export interface CustomRequestInit {
-  /**
-   * A BodyInit object or null to set request's body.
-   */
-  body?: BodyInit | null;
-  /**
-   * A string indicating how the request will interact with the browser's cache to set request's cache.
-   */
-  cache?: RequestCache;
-  /**
-   * A string indicating whether credentials will be sent with the request always, never, or only when sent to a same-origin URL. Sets request's credentials.
-   */
-  credentials?: RequestCredentials;
-  /**
-   * A Headers object, an object literal, or an array of two-item arrays to set request's headers.
-   */
-  headers?: CustomHeadersInit;
-  /**
-   * A cryptographic hash of the resource to be fetched by request. Sets request's integrity.
-   */
-  integrity?: string;
-  /**
-   * A boolean to set request's keepalive.
-   */
-  keepalive?: boolean;
-  /**
-   * A string to set request's method.
-   */
-  method?: string;
-  /**
-   * A string to indicate whether the request will use CORS, or will be restricted to same-origin URLs. Sets request's mode.
-   */
-  mode?: RequestMode;
-  /**
-   * A string indicating whether request follows redirects, results in an error upon encountering a redirect, or returns the redirect (in an opaque fashion). Sets request's redirect.
-   */
-  redirect?: RequestRedirect;
-  /**
-   * A string whose value is a same-origin URL, "about:client", or the empty string, to set request's referrer.
-   */
-  referrer?: string;
-  /**
-   * A referrer policy to set request's referrerPolicy.
-   */
-  referrerPolicy?: ReferrerPolicy;
-  /**
-   * An AbortSignal to set request's signal.
-   */
-  signal?: AbortSignal | null;
-  /**
-   * Can only be null. Used to disassociate request from any Window.
-   */
-  window?: any;
-}
+type CustomRequestInit = Override<
+  RequestInit,
+  { headers?: Headers | string[][] | Record<string, string | undefined> | undefined }
+>;
 
 export interface FetchArgs extends CustomRequestInit {
   url: string;
@@ -104,6 +40,17 @@ const handleResponse = async (response: Response, responseHandler: ResponseHandl
 export interface FetchBaseQueryError {
   status: number;
   data: unknown;
+}
+
+function cleanUndefinedHeaders(headers: any) {
+  if (!isPlainObject(headers)) {
+    return headers;
+  }
+  const copy: Record<string, any> = { ...headers };
+  for (const [k, v] of Object.entries(copy)) {
+    if (typeof v === 'undefined') delete copy[k];
+  }
+  return copy;
 }
 
 export function fetchBaseQuery({
@@ -139,19 +86,13 @@ export function fetchBaseQuery({
       ...rest,
     };
 
-    let tempHeaders = prepareHeaders(new Headers(headers), { getState });
+    config.headers = prepareHeaders(new Headers(cleanUndefinedHeaders(headers)), { getState });
 
-    tempHeaders.forEach((value, key) => {
-      if (value === 'undefined') {
-        tempHeaders.delete(key);
-      }
-    });
-
-    if (!tempHeaders.has('content-type')) {
-      tempHeaders.set('content-type', 'application/json');
+    if (!config.headers.has('content-type')) {
+      config.headers.set('content-type', 'application/json');
     }
 
-    if (body && isPlainObject(body) && isJsonContentType(tempHeaders)) {
+    if (body && isPlainObject(body) && isJsonContentType(config.headers)) {
       config.body = JSON.stringify(body);
     }
 
@@ -162,8 +103,6 @@ export function fetchBaseQuery({
     }
 
     url = joinUrls(baseUrl, url);
-
-    config.headers = tempHeaders as any;
 
     const response = await fetchFn(url, config);
     const resultData = await handleResponse(response, responseHandler);

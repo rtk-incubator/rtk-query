4bf53aa1bbfd245d7abcdf0cace84737a7525f66
Lenz Weber <mail@lenzw.de>
2021-03-27 17:31:18 +0100

    fixup test

--- test/buildMiddleware.test.tsx
+++ test/buildMiddleware.test.tsx
@@ -30,7 +30,7 @@ it('invalidates the specified entities', async () => {
   await storeRef.store.dispatch(getBanana.initiate(1));
   matchSequence(storeRef.store.getState().actions, getBanana.matchPending, getBanana.matchFulfilled);
 
-  await storeRef.store.dispatch(api.internalActions.invalidateEntities(['Banana', 'Bread']));
+  await storeRef.store.dispatch(api.util.invalidateEntities(['Banana', 'Bread']));
 
   // Slight pause to let the middleware run and such
   await waitMs(20);
@@ -38,14 +38,14 @@ it('invalidates the specified entities', async () => {
   const firstSequence = [
     getBanana.matchPending,
     getBanana.matchFulfilled,
-    api.internalActions.invalidateEntities.match,
+    api.util.invalidateEntities.match,
     getBanana.matchPending,
     getBanana.matchFulfilled,
   ];
   matchSequence(storeRef.store.getState().actions, ...firstSequence);
 
   await storeRef.store.dispatch(getBread.initiate(1));
-  await storeRef.store.dispatch(api.internalActions.invalidateEntities([{ type: 'Bread' }]));
+  await storeRef.store.dispatch(api.util.invalidateEntities([{ type: 'Bread' }]));
 
   await waitMs(20);
 
@@ -54,7 +54,7 @@ it('invalidates the specified entities', async () => {
     ...firstSequence,
     getBread.matchPending,
     getBread.matchFulfilled,
-    api.internalActions.invalidateEntities.match,
+    api.util.invalidateEntities.match,
     getBread.matchPending,
     getBread.matchFulfilled
   );
@@ -62,21 +62,21 @@ it('invalidates the specified entities', async () => {
 
 describe.skip('TS only tests', () => {
   it('should allow for an array of string EntityTypes', () => {
-    api.internalActions.invalidateEntities(['Banana', 'Bread']);
+    api.util.invalidateEntities(['Banana', 'Bread']);
   });
   it('should allow for an array of full EntityTypes descriptions', () => {
-    api.internalActions.invalidateEntities([{ type: 'Banana' }, { type: 'Bread', id: 1 }]);
+    api.util.invalidateEntities([{ type: 'Banana' }, { type: 'Bread', id: 1 }]);
   });
 
   it('should allow for a mix of full descriptions as well as plain strings', () => {
-    api.internalActions.invalidateEntities(['Banana', { type: 'Bread', id: 1 }]);
+    api.util.invalidateEntities(['Banana', { type: 'Bread', id: 1 }]);
   });
   it('should error when using non-existing EntityTypes', () => {
     // @ts-expect-error
-    api.internalActions.invalidateEntities(['Missing Entity']);
+    api.util.invalidateEntities(['Missing Entity']);
   });
   it('should error when using non-existing EntityTypes in the full format', () => {
     // @ts-expect-error
-    api.internalActions.invalidateEntities([{ type: 'Missing' }]);
+    api.util.invalidateEntities([{ type: 'Missing' }]);
   });
 });
